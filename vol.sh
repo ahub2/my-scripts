9a2af58acfaa83b4dd9ff5eabdabcd0c60fd6b18
Alex <alex@ahubbard.xyz>
2022-09-22 23:35:47 -0500

    add wob to install, add mywob script to repo

--- vol.sh
+++ vol.sh
@@ -6,12 +6,23 @@ refbar() {
     pkill sleep -P "$(cat ~/.cache/statusbar_pid )"
 }
 
+display() {
+    VAL="$(get | sed 's/^.*\.//g' )"
+
+    [ -n "$1" ] && VAL="$1"
+
+    mywob "$VAL"
+
+}
+
 inc() {
     [ "$1" ] && PERC="$1"
     #pulsemixer --change-volume +"$1" && refbar 
     #amixer sset Master 1%+  && refbar 
     #pamixer -i "$PERC" && refbar
     wpctl set-volume @DEFAULT_AUDIO_SINK@ "$(echo "$(get) + $PERC" | bc)" && refbar
+
+    display
 }
 
 dec() {
@@ -20,6 +31,8 @@ dec() {
     #amixer sset Master 1%-  && refbar 
     #pamixer -d "$PERC" && refbar
     wpctl set-volume @DEFAULT_AUDIO_SINK@ "$(echo "$(get) - $PERC" | bc)" && refbar
+
+    display
 }
 
 mute() {
@@ -27,6 +40,8 @@ mute() {
     #amixer sset Master toggle && refbar 
     #pamixer -t && refbar
     wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle && refbar 
+
+    display
 }
 
 get() {
@@ -48,6 +63,8 @@ elif  [ "$1" = "--toggle-mute" ]; then
     mute
 elif [ "$1" = "-g" ]; then
     get
+elif [ "$1" = "-display" ]; then
+    display
 else
     help
 fi
