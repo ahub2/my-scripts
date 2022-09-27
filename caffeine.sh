d92364515866b144d16920d06a1bd0dc98e51487
alex <alex@ahubbard.xyz>
2022-09-26 21:15:28 -0500

    make caffeine.sh work again add wayland-inhibit-idle-git to aur.list some changes to trans-add

--- caffeine.sh
+++ caffeine.sh
@@ -1,8 +1,33 @@
 #!/bin/sh
 
-while : 
-do
-    wtype -d 1000 caffeine 
-    wtype -d 1000 -k return 
-    sleep 60
-done
+PROGRAM_NAME="wayland-idle-inhibitor.py"
+
+help() {
+    echo "use to toggle inhibiting idle on wayland"
+    echo ""
+    echo "usage: caffeine.sh [OPTIONS]"
+    echo ""
+    echo "    -i  -  initialize and begin inhibiting idle, which will stop the computer from sleeping." 
+    echo "    -s  -  get status of program, return ON if idle is inhibited, or OFF if idle is not inhibited"
+    echo "    -k  -  kill program inhibiting idle, allowing the computer to sleep" 
+    echo ""
+    
+    exit
+}
+
+if [ "$1" = "-i" ]; then
+    $PROGRAM_NAME
+
+elif [ "$1" = "-s" ]; then
+    if [ "$(pgrep -f $PROGRAM_NAME)" ]; then
+        echo "ON"
+    else
+        echo "OFF"
+    fi
+elif [ "$1" = "-k" ]; then
+    pkill -f "$PROGRAM_NAME" 
+else
+    help
+fi
+
+wait
