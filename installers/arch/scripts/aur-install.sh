0329f03e8b217fa292a0c8596e240ac27605cc08
Alex <alex@ahubbard.xyz>
2023-01-07 20:16:01 -0600

    add greetd setup to repo

--- installers/arch/scripts/aur-install.sh
+++ installers/arch/scripts/aur-install.sh
@@ -12,6 +12,19 @@ yay_install() {
     makepkg -si
 }
 
+greetd_config() {
+    sudo mkdir -p /etc/greetd/
+
+GREETD_CONFIG="[terminal]
+vt = 1
+
+[default_session]
+command = \"tuigreet --time --cmd sway\"
+user = \"greeter\""
+
+    sudo sh -c "echo $GREETD_CONFIG > /etc/greetd/config.toml" 
+}
+
 pkg_install() {
     aurprogs="$(cat "$AUR_PROGS_FILE")"
     for pkg in $aurprogs
@@ -25,6 +38,8 @@ if [ -f "$AUR_PROGS_FILE" ]; then
     ! [ -f "/usr/bin/yay" ] && yay_install
 
     pkg_install
+
+    
 
 else
     echo "enter file containing package names to install separated by newlines as first input argument" && exit
---
+++ installers/arch/scripts/greetd-config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+yay -S greetd greetd-tuigreet
+
+GREETD_CONFIG="[terminal]
+vt = 1
+
+[default_session]
+command = \"tuigreet --time --cmd sway\"
+user = \"greeter\""
+
+sudo mkdir -p /etc/greetd/
+sudo sh -c "echo $GREETD_CONFIG > /etc/greetd/config.toml"
