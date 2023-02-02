b0e94c0727719ae52bb9a950ee724094d5142730
Alex <alex@ahubbard.xyz>
2023-02-02 09:04:38 -0600

    add gsudo script to scripts directory. this script allows you to run graphical programs as root. add requirement for gsudo to PKGBUILD. update opendoas to install doas-sudo-shim since previous package no longer exists.

--- installers/arch/scripts/opendoas.sh
+++ installers/arch/scripts/opendoas.sh
@@ -6,4 +6,4 @@ echo "permit nopass :wheel as root cmd /sbin/poweroff" >> ~/.cache/doas.conf
 echo "permit nopass :wheel as root cmd /sbin/reboot" >> ~/.cache/doas.conf
 sudo cp ~/.cache/doas.conf /etc/doas.conf
 
-yay -S opendoas-sudo
+yay -S doas-sudo-shim
