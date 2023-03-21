29aca47aa36ab302c4a395789208e46c7b223654
Alex <alex@ahubbard.xyz>
2023-03-20 23:02:09 -0500

    *remove allowing poweroff and reboot in doas.conf since it is not needed
    *add pass dir and gnupg dir to setup-dirs.sh script
    
    *remove some lines from harden.sh

--- installers/arch/scripts/opendoas.sh
+++ installers/arch/scripts/opendoas.sh
@@ -2,8 +2,8 @@
 sudo pacman -S opendoas
 
 echo "permit persist $USER as root" > ~/.cache/doas.conf
-echo "permit nopass :wheel as root cmd /sbin/poweroff" >> ~/.cache/doas.conf
-echo "permit nopass :wheel as root cmd /sbin/reboot" >> ~/.cache/doas.conf
+#echo "permit nopass :wheel as root cmd /sbin/poweroff" >> ~/.cache/doas.conf
+#echo "permit nopass :wheel as root cmd /sbin/reboot" >> ~/.cache/doas.conf
 sudo cp ~/.cache/doas.conf /etc/doas.conf
 
 yay -S doas-sudo-shim
